54badaf1eef3675364fd7bf17741382a5a0a968d
Edd Dumbill <edumbill+github@gmail.com>
2016-12-09 11:20:37 -0800

    Don't bother tracking the string in part 1

--- 09/main.clj
+++ 09/main.clj
@@ -6,20 +6,16 @@
 (defn next-chunk [s]
   (rest (first (re-seq #"^(\w*)\((\d+)x(\d+)\)(.*)$" s))))
 
-(defn expand [s]
+(defn expand-count [s]
   (let [m (next-chunk s)]
-    (if (empty? m) [s "" ""]
-      (let [p (first m) n (read-string (second m))
+    (if (empty? m) (count s)
+      (let [p (count (first m))
+            n (read-string (second m))
             o (read-string (nth m 2))
-            q (str/join (flatten (repeat o (take n (last m)))))
             r (str/join (drop n (last m)))]
-        [p q r]))))
+        (+ p (* o n) (expand-count r))))))
 
-(defn expand-code [s]
-  (loop [r "" s s]
-    (if (= s "") r (let [[d e f] (expand s)] (recur (str/join [r d e]) f)))))
-
-(defn part-one [] (count (expand-code inp)))
+(defn part-one [] (expand-count inp))
 
 ; part 2
 
