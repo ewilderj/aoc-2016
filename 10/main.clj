b5ad80f50f9ae6af0fd363e48357c1efa30d0366
Edd Dumbill <edumbill+github@gmail.com>
2016-12-10 13:38:11 -0800

    cleanups

--- 10/main.clj
+++ 10/main.clj
@@ -4,15 +4,8 @@
 (def inp (str/split-lines (slurp "/Users/edd/git/aoc-2016/10/puzzle.txt")))
 
 (defn assign [a t n v]
-  (if (= t :output)
-    (assoc-in a [:output n] (sort (cons v (get-in a [:output n] []))))
-    (let [ap (assoc-in a [:bot n] (sort (cons v (get-in a [:bot n] []))))]
-      (check-and-exec ap n))))
-
-(defn give-rule [bot [lw ln] [hw hn]]
-  (fn [a]
-    (let [[l h] (get-in a [:bot bot] [])]
-      (assign (assign a lw ln l) hw hn h))))
+  (let [ap (assoc-in a [t n] (sort (cons v (get-in a [t n] []))))]
+    (if (= t :bot) (check-and-exec ap n) ap)))
 
 (defn check-and-exec [a bot]
   (let [vs (get-in a [:bot bot]) r (get-in a [:rules bot])]
@@ -20,6 +13,10 @@
       (do (if (= [17 61] vs) (println "bot" bot "has two values" vs))
           (r a))
       a)))
+
+(defn give-rule [bot [lw ln] [hw hn]]
+  (fn [a] (let [[l h] (get-in a [:bot bot] [])]
+            (assign (assign a lw ln l) hw hn h))))
 
 (defn parse-line [a l]
   (let [ws (str/split l #"\s+")]
@@ -31,14 +28,11 @@
             hw (keyword (nth ws 10)) hn (Integer. (nth ws 11))]
         (assoc-in a [:rules b] (give-rule b [lw ln] [hw hn]))))))
 
-(defn make-machine []
-  (loop [i (filter #(str/starts-with? % "bot") inp) a {}]
-    (if (empty? i) a (recur (rest i) (parse-line a (first i))))))
+(defn parse [i a]
+  (loop [i i a a] (if (empty? i) a (recur (rest i) (parse-line a (first i))))))
 
-(defn assign-values [a]
-  (loop [i (filter #(str/starts-with? % "value") inp) a a]
-    (if (empty? i) a (recur (rest i) (parse-line a (first i))))))
+(defn make-machine [] (parse (filter #(str/starts-with? % "bot") inp) {}))
+(defn assign-values [a] (parse (filter #(str/starts-with? % "value") inp) a))
 
 (defn part-one [] (assign-values (make-machine)))
-
-(defn part-two []) (reduce * (flatten (map ((part-one) :output) [0 1 2])))
+(defn part-two [] (reduce * (flatten (map ((part-one) :output) [0 1 2]))))
