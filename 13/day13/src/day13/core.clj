e8ae068b9211d07c1f7a8e3fbc52b19d9eab1c73
Edd Dumbill <edumbill+github@gmail.com>
2016-12-13 15:28:13 -0800

    part two solved

--- 13/day13/src/day13/core.clj
+++ 13/day13/src/day13/core.clj
@@ -14,6 +14,11 @@
     (for [y (range n)]
       (str/join (map #(if % \# \.) (map #(is-wall? % y m) (range n)))))))
 
+(defn draw-grid2 [n m been]
+  (str/join "\n"
+    (for [y (range n)]
+      (str/join (map #(if (been [% y]) \O (if (is-wall? % y m) \# \.)) (range n))))))
+
 (defn estimate [x y tx ty] (+ (Math/abs (- x tx)) (Math/abs (- y ty))))
 
 (defn best-choice [s f] (first (sort-by f s)))
@@ -39,25 +44,35 @@
 (defn solve [tx ty gx gy m]
   (loop [closed #{} open #{[tx ty]} came-from {} gscore {[tx ty] 0}
          fscore {[tx ty] (estimate tx ty gx gy)} n 0]
-    (if (or (> n 1000) (empty? open))
-      "FAIL"
-      (let [cur (best-choice open fscore)]
-        (if (= cur [gx gy])
-            (do (println "WIN")
-                (unwind-path came-from gx gy))
-              ; not won yet, now let's dig in
-            (let [nc (conj closed cur)
-                  [x y] cur
-                  cn (set/difference (open-neighbors x y m) closed)
-                  no (set/union cn (remove #{cur} open))
-                  tgs (into {} (map #(vec [% (tent-gscore cur % gscore)]) cn))
-                  np (set (filter #(or (not (gscore %)) (< (tgs %) (gscore %))) cn))
-                  ng (merge gscore (select-keys tgs np))
-                  nf (merge fscore (into {} (map #(vec [% (new-fscore % [gx gy] ng)]) np)))
-                  ncf (merge came-from (into {} (map #(vec [% cur]) np)))]
+    (if (or (> n 200) (empty? open))
+      ; n is a safety valve for when i screw up and infinitely recurse
+     (println "FAILED IN " (str n))
+     (let [cur (best-choice open fscore)]
+       (if (= cur [gx gy])
+           (unwind-path came-from gx gy)
+         (let [nc (conj closed cur)
+               [x y] cur
+               cn (set/difference (open-neighbors x y m) closed)
+               no (set/union cn (remove #{cur} open))
+               tgs (into {} (map #(vec [% (tent-gscore cur % gscore)]) cn))
+               np (set (filter #(or (not (gscore %)) (< (tgs %) (gscore %))) cn))
+               ng (merge gscore (select-keys tgs np))
+               nf (merge fscore (into {} (map #(vec [% (new-fscore % [gx gy] ng)]) np)))
+               ncf (merge came-from (into {} (map #(vec [% cur]) np)))]
 
-              ; for each candidate neighbor cn...
-              (recur nc no ncf ng nf (inc n))))))))
+          (recur nc no ncf ng nf (inc n))))))))
 
-(def part-one (count (solve 1 1 31 39 1364)))
-(def part-two )
+(defn part-one [] (count (solve 1 1 31 39 1364)))
+
+(defn solve2 [tx ty lim m]
+  (loop [queue [[tx ty]] dist {[tx ty] 0}]
+    (if (or (>= (dist (first queue)) lim) (empty? queue))
+      dist
+      (let [cur (first queue)
+            [x y] cur
+            cn (filter #(not (dist %)) (open-neighbors x y m))
+            nd (merge (into {} (map #(vec [% (inc (dist cur))]) cn)) dist)
+            nq (concat (rest queue) cn)]
+          (recur nq nd)))))
+
+(defn part-two [] (count (solve2 1 1 50 1364)))
