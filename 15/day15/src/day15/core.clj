584af3fa5a739482f3b8df7228a6a4569df30f33
Edd Dumbill <edumbill+github@gmail.com>
2016-12-15 09:37:58 -0800

    Make briefer with for loop, thanks xgavin

--- 15/day15/src/day15/core.clj
+++ 15/day15/src/day15/core.clj
@@ -3,8 +3,7 @@
 (def discs [[13 1] [19 10] [3 2] [7 1] [5 3] [17 5]])
 (defn d-at-t? [[m o] t] (= 0 (mod (+ o t) m)))
 (defn win? [ds t]
-  (every? identity (map #(d-at-t? (second %) (+ (first %) (inc t))) (partition 2 (interleave (range) ds)))))
-(defn find-winner [d]
-  (first (filter some? (for [i (range)] (if (win? d i) i nil)))))
+  (every? identity (for [x (range (count ds))] (d-at-t? (nth ds x) (+ t x 1)))))
+(defn find-winner [d] (first (filter some? (for [i (range)] (if (win? d i) i nil)))))
 (defn part-one [] (find-winner discs))
 (defn part-two [] (find-winner (conj discs [11 0])))
