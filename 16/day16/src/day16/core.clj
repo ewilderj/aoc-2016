c4afc615ba87a54156be83c0d9a94fdb31bd546f
Edd Dumbill <edumbill+github@gmail.com>
2016-12-16 10:21:43 -0800

    Reverting to more performant algo

--- 16/day16/src/day16/core.clj
+++ 16/day16/src/day16/core.clj
@@ -1,15 +1,16 @@
 (ns day16.core
   (:require [clojure.string :as str]))
 
-(defn swap [x] (map {\0 \1 \1 \0} x))
-(defn dragon [x] (apply concat [x "0" (swap (reverse x))]))
+(defn swap [x] (str/join (map #(if (= \0 %) \1 \0) x)))
+(defn dragon [x] (str/join [x "0" (swap (reverse x))]))
 (defn dragons [x n]
-  (take n (loop [out x]
-            (if (>= (count out) n) out (recur (dragon out))))))
-(defn parity [x] (map #(if (apply = %) \1 \0) (partition 2 x)))
+  (str/join (take n (loop [out x]
+                      (if (>= (count out) n) out
+                        (recur (dragon out)))))))
+(defn parity [x] (str/join (map #(if (apply = %) \1 \0) (partition 2 x))))
 (defn checksum [x]
   (loop [c (parity x)]
-    (if (odd? (count c)) (apply str c) (recur (parity c)))))
+    (if (odd? (count c)) c (recur (parity c)))))
 (defn example-q [] (checksum (dragons "10000" 20)))
 (defn part-one [] (checksum (dragons "01110110101001000" 272)))
 (defn part-two [] (checksum (dragons "01110110101001000" 35651584)))
