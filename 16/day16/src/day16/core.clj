e69a09981cf6d5faaba9076f95fb0ba55443216a
Edd Dumbill <edumbill+github@gmail.com>
2016-12-19 07:14:59 -0800

    Day 18

--- 16/day16/src/day16/core.clj
+++ 16/day16/src/day16/core.clj
@@ -8,9 +8,7 @@
                       (if (>= (count out) n) out
                         (recur (dragon out)))))))
 (defn parity [x] (str/join (map #(if (apply = %) \1 \0) (partition 2 x))))
-(defn checksum [x]
-  (loop [c (parity x)]
-    (if (odd? (count c)) c (recur (parity c)))))
+(defn checksum [x] (loop [c (parity x)] (if (odd? (count c)) c (recur (parity c)))))
 (defn example-q [] (checksum (dragons "10000" 20)))
 (defn part-one [] (checksum (dragons "01110110101001000" 272)))
 (defn part-two [] (checksum (dragons "01110110101001000" 35651584)))
