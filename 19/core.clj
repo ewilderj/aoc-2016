d01015aa97c522ea0a879092a02580d09da8adfc
Edd Dumbill <edumbill+github@gmail.com>
2016-12-20 11:26:58 -0800

    Part 2 with a solution

--- 19/core.clj
+++ 19/core.clj
@@ -1,9 +1,13 @@
 (ns day19.core
   (:require [clojure.string :as str]))
 
+; (defn del-k [e k]
+;   (if (= k (count e)) (rest e)
+;     (keep-indexed #(if (not (= %1 k)) %2) e)))
+
 (defn del-k [e k]
   (if (= k (count e)) (rest e)
-    (keep-indexed #(if (not (= %1 k)) %2) e)))
+    (vec (concat (subvec e 0 k) (subvec e (inc k))))))
 
 (defn slow-part-one [elves]
   (loop [e (vec (range elves)) k 1]
@@ -14,25 +18,30 @@
 (defn part-one [elves]
   (inc (* 2 (read-string (apply str "2r" (apply str (rest (Integer/toBinaryString elves))))))))
 
+(defn rotate [l r]
+  (let [nl (if (< (count r) (count l)) (drop-last l) l)
+        nr (if (< (count r) (count l)) r (rest r))
+        ql (conj (vec (rest nl)) (first nr))
+        qr (conj (vec (rest nr)) (first nl))]
+    [ql qr]))
 
-; part 2 still unsolved
-(defn choose-vic [n k] k
-  (mod (int (+ (/ n 2) k)) n))
+(defn split-list-two [n]
+  (let [m (quot n 2) l (range 0 m) r (range m n)]
+    (loop [l l r r]
+      (if (or (nil? (first l)) (nil? (first r)))
+        [l r]
+        (let [[nl nr] (rotate l r)]
+          (recur nl nr))))))
 
-(defn slow-part-two [elves]
-  (loop [e (vec (range elves)) k 0 n 0]
-;    (println k e n)
-    (if (= 1 (count e)) (first e)
-      (let [v (choose-vic (count e) k)
-            nk (if (>= (inc k) (dec (count e))) 0 (inc k))]
-        (recur (del-k e v) nk (inc n))))))
+; executes in hideous time, use linear method instead
+(defn split-part-two [] (inc (first (filter some? (flatten (split-list-two 5))))))
 
-(defn pad [n]
-  (apply str (take-last 8 (str/join ["0000000" (Integer/toBinaryString n)]))))
+; uses the progression to move from the answer to the next one
+(defn linear-part-two [n]
+  (loop [p 2 i 4]
+    (if (> i n) p
+      (let [np (mod (inc p) (dec i))
+            qp (if (>= np (quot i 2)) (inc np) np)]
+        (recur qp (inc i))))))
 
-(defn pattern []
-  (for [n (rest (range 129))]
-    (let [m (slow-part-two n)]
-      (apply str [n " -> " m ": " (pad n) " -> " (pad m) " - "
-                ;  (- n m)
-                  "\n"]))))
+(defn part-two [] (inc (linear-part-two 3014603)))
